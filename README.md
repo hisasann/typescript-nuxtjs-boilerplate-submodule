ec10af5e62880be299e44179e2bd7d4acfc57548
hisasann <hisasann25@gmail.com>
2019-03-19 17:20:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# typescript-nuxtjs-boilerplate-submodule
---
+++ include.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div
+    h1.title
+      | include
+    | {{ message }}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Include extends Vue {
+  message: string = 'hello include'
+
+  beforeCreate() {
+    console.log('beforeCreate')
+  }
+  created() {
+    console.log('created')
+  }
+
+  public head() {
+    return {
+      title: 'hello include'
+    }
+  }
+}
+</script>
